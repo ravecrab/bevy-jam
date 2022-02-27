e26d85888eb359a5b39119b85a16d728f1ee2b15
Ross Schulman <ross@rbs.io>
2022-02-27 13:49:34 -0500

    Add some basic components and empty ui.rs file

---
+++ src/cards/mod.rs
@@ -0,0 +1,25 @@
+import bevy::prelude::*;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Speed(u32);
+
+#[derive(Component)]
+struct Attack(u32);
+
+#[derive(Component)]
+struct Hitpoints(u32);
+
+#[derive(Component)]
+struct Deck;
+
+#[derive(Component)]
+struct Hand;
+
+#[derive(Component)]
+struct Card;
+
+#[derive(Component)]
+struct InPlay;
